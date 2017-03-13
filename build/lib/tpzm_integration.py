98961f46a1d2a61382b817e56821146b29cdb6a8
ZhangHuiyuan <zhanghuiyuanses@126.com>
2017-03-13 21:22:40 +0800

    upgrade to 1.0.3

--- build/lib/tpzm_integration.py
+++ build/lib/tpzm_integration.py
@@ -3,11 +3,11 @@ print('This program calculates the integral of input function from a to b')
 print('by cutting it into many parts and calculating each area')
 print('Program written by Zhang Huiyuan')
 print('from Shanghai Guanghua Cambridge International School')
-print('use tpzm_approx(a,b,N,func) to call function')
+print('use integral_approx(a,b,N,func) to call function')
 print('add+ subtract- multiply* divide/ power**')
-print('Shortcut: xxxx=tpzm_integration.tpzm_approx')
+print('Shortcut: xxxx=tpzm_integration.integral_approx')
 print('then use xxxx to substitute the long name')
-def tpzm_approx(a=1,b=2,N=1,func='1.0/x'):
+def integral_approx(a=1,b=2,N=1,func='1.0/x'):
     try:
         a=float(a)
         b=float(b)
@@ -15,8 +15,10 @@ def tpzm_approx(a=1,b=2,N=1,func='1.0/x'):
         #Function to evaluate
         func
         iterate1=1
-        total_area=0
+        total_area_1=0
+        total_area_2=0
         while iterate1 <= N:
+            #the trapezium way
             left_x=a+((b-a)*(iterate1-1)/N)
             right_x=a+((b-a)*iterate1/N)
             #print iterate1
@@ -25,9 +27,18 @@ def tpzm_approx(a=1,b=2,N=1,func='1.0/x'):
             left_y=eval(func)
             x=right_x
             right_y=eval(func)
-            delta_area=(right_x - left_x)*(left_y + right_y)/2
-            total_area=total_area+delta_area
+            delta_area_1=(right_x - left_x)*(left_y + right_y)/2
+            total_area_1=total_area_1+delta_area_1
+            #the rectangle way
+            x=(left_x+right_x)/2
+            y=eval(func)
+            delta_area_2=(right_x - left_x)*y
+            total_area_2=total_area_2+delta_area_2
             iterate1=iterate1+1
-        return total_area
+        return 'Trapezium way ',total_area_1,' Rectangle way ',total_area_2
     except ZeroDivisionError:
-        return 'Cannot divide things by zero!!'
+        return "ErrorM1 Division by Zero ERROR!"
+    except NameError:
+        return "ErrorM2 Don't use undefined variables!"
+    except ValueError:
+        return "ErrorM3 Don't use strings in first 3 inputs!"
--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name = 'tpzm_integration',
-    version = '1.0.2',
+    version = '1.0.3',
     py_modules = ['tpzm_integration'],
     author = 'ZhangHuiYuanEric',
     author_email = 'zhanghuiyuanses@126.com',
     url = 'https://github.com/ZhangHuiyuan',
-    description = 'A program that evaluates fixed integrals with trapeziums',
+    description = 'A program that evaluates fixed integrals with trapeziums and rectangles',
     )
